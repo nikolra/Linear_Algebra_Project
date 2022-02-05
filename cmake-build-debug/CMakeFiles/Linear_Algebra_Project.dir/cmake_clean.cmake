892427e2e87956e4a720ecb98d38a155a7fd749b
Margarita Gurevich <ritagurevich1996@gmail.com>
2022-02-05 08:47:06 +0200

    question 10

---
+++ cmake-build-debug/CMakeFiles/Linear_Algebra_Project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Linear_Algebra_Project.dir/main.cpp.obj"
+  "CMakeFiles/Linear_Algebra_Project.dir/main.cpp.obj.d"
+  "Linear_Algebra_Project.exe"
+  "Linear_Algebra_Project.exe.manifest"
+  "Linear_Algebra_Project.pdb"
+  "libLinear_Algebra_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Linear_Algebra_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Linear_Algebra_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Linear_Algebra_Project.
+# This may be replaced when dependencies are built.
